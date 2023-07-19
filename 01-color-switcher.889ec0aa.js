b93f944483dfc48644b531e9b3d966620b8aa938
Julipusik <125054492+Julipusik@users.noreply.github.com>
2023-07-19 18:54:26 +0000

    Deploying to gh-pages from @ Julipusik/goit-js-hw-09@0d72aa3cdff805c2d595e9705612f464ead66965 🚀

---
+++ 01-color-switcher.889ec0aa.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]");function n(){const t=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`;document.body.style.backgroundColor=t}let o;t.addEventListener("click",(function(){t.disabled=!0,o=setInterval(n,1e3)})),e.addEventListener("click",(function(){t.disabled=!1,clearInterval(o)}));
+//# sourceMappingURL=01-color-switcher.889ec0aa.js.map
